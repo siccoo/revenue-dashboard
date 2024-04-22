32e1b8a7a348901dcbf4bb472f21763bddec1166
Chilaka Michael Obinna <obichilaka@gmailcom>
2024-04-22 16:08:40 +0100

    nav & sidebar icons updates

---
+++ src/assets/index.tsx
@@ -0,0 +1,8 @@
+export { default as logo } from './mainstack-logo.svg';
+export { default as close } from './navClose.svg';
+export { default as arrowDown } from './navArrowDown.svg';
+export { default as link } from './sideBarProductIcons.svg';
+export { default as store } from './iconOne.svg';
+export { default as media } from './iconTwo.svg';
+export { default as invocing } from './iconThree.svg';
+export { default as right } from './arrow-right-s-line.svg';
